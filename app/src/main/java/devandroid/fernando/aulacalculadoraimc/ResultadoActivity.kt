35409c2b760f664c018969b2c0ef68625e4eae55
xF3rd <fernando.bordin.correa@gmail.com>
2024-09-22 20:09:17 -0300

    Initial commit + Layout feito

---
+++ app/src/main/java/devandroid/fernando/aulacalculadoraimc/ResultadoActivity.kt
@@ -0,0 +1,16 @@
+package devandroid.fernando.aulacalculadoraimc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+    }
+}
